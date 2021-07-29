220b88aa157f4a18300f6f34cbe2fd17fdc0e248
Gabriel Martins Carossi <gabrielmcarossi@gmail.com>
2021-07-28 21:15:10 -0300

    Implementacao de programa com o uso de interface

---
+++ Prj_AulaInterfaces/Entities/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace Prj_AulaInterfaces.Entities
+{
+    class Vehicle
+    {
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
---
+++ Prj_AulaInterfaces/Services/ITaxService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prj_AulaInterfaces.Services
+{
+    interface ITaxService
+    {
+        double Tax(double amount);
+    }
+}
